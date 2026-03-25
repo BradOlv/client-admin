a41b7d538e140a0b2ddf4a6d10956dbaa0eab378
BradOlv <boliva-2023502@kinal.edu.gt>
2026-03-25 11:58:30 -0600

    feat: subiendo proyecto completo Kinal Sports (Client y Server)

---
+++ client-admin/src/app/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-5xl font-extrabold text-blue-600 drop-shadow-md">
+        ¡Hola Mundo!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ client-admin/src/app/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from '@material-tailwind/react';
+import { BrowserRouter } from 'react-router-dom';
+
+import '../styles/index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </React.StrictMode>
+)
